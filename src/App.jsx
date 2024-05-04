3405f5cb65ad5bcc69b6d7a50a3bf5713b6ea207
Nazil Ashraf C <nazil.ashraf@digital.datamatics.com>
2024-05-04 09:47:32 +0530

    completed the development of parent component

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.scss';
+import Card from './component/Card';
+import Filter from './component/Filter';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchApiJSON } from './redux/slice/fetchingAPISlice';
+import Loading from './atoms/loading';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const dispatch = useDispatch();
+  const [page, setPage] = useState(12);
+
+  // access state from redux
+  const { data, loading } = useSelector(state => state.JsonData);
+  useEffect(() => {
+    // fetch  Api
+    dispatch(fetchApiJSON(page));
+  }, [dispatch, page]);
+
+  //infinite scrolling
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop >=
+      document.documentElement.offsetHeight - 50
+    ) {
+      setPage(prevPage => prevPage + 12);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   return (
-    <>
-      <div>
-        
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className='page-container'>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <Filter />
+          <Card Apidata={data?.jdList} />
+        </>
+      )}
+    </div>
+  );
+};
 
-export default App
+export default App;
